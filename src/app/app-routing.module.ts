7dd20284e8988e6d280f96bd3e3174f6340ee414
verge <vergelbarit35@gmail.com>
2017-11-29 10:55:20 +0800

    added mockdata

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from '../app/component/login/login.component';
+import { HomeComponent } from '../app/component/home/home.component';
+import { AuthGuard} from '../app/guards/auth.guard';
 const routes: Routes = [
   { path: 'Login', component: LoginComponent },
+  { path: '', component: HomeComponent, canActivate: [AuthGuard]},
+      // otherwise redirect to home
+      { path: '**', redirectTo: '' }
 ]
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,35 +6,40 @@ import { HttpModule} from '@angular/http';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+//pages
+import { IcoComponent } from './ico/page/ico/ico.component';
+import { HomeComponent } from './component/home/home.component';
+
 //Material Design
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material.module';
 import { NavtoolbarComponent } from './component/navtoolbar/navtoolbar.component';
 import { IcoModuleRouting } from './ico/routes/ico/ico.routing.module';
-import { IcoComponent } from './ico/page/ico/ico.component';
 import { IcoService } from './ico/service/ico.service';
 //fake backedn 
 import { fakeBackendProvider } from '../app/helpers/fakebackend';
 import { LoginComponent } from './component/login/login.component';
 import { AuthenticationService } from './authentication/authentication.service';
-
+import { MockBackend } from '@angular/http/testing';
+import { AuthGuard } from './guards/auth.guard';
 @NgModule({
   declarations: [
     AppComponent,
     NavtoolbarComponent,
     IcoComponent,
-    LoginComponent
+    LoginComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     BrowserAnimationsModule,
     MaterialModule,
     FormsModule,
+    AppRoutingModule,
     IcoModuleRouting,
     HttpModule
   ],
-  providers: [IcoService, fakeBackendProvider, AuthenticationService],
+  providers: [IcoService, fakeBackendProvider, AuthenticationService, MockBackend, AuthGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
